c3e354b57d6e541e45fb77fcdae6b5051757f993
Gregory Igelmund <gregory.igelmund@gmail.com>
2013-12-16 12:57:14 +0100

    added cache_removal and cache_size_limit

--- lib/brandeins/config.rb
+++ lib/brandeins/config.rb
@@ -4,11 +4,12 @@ require 'pathname'
 #
 module BrandEins
   class Config
-    BASE_URI    = 'http://www.brandeins.de'
-    ARCHIVE_URI = BASE_URI + '/archiv.html'
-    BASE_PATH   = Pathname.new(ENV['HOME']) + '.brandeins'
-    CACHE_PATH  = BASE_PATH + 'cache'
-    TEMP_PATH   = BASE_PATH + 'temp'
+    BASE_URI          = 'http://www.brandeins.de'
+    ARCHIVE_URI       = BASE_URI + '/archiv.html'
+    BASE_PATH         = Pathname.new(ENV['HOME']) + '.brandeins'
+    CACHE_PATH        = BASE_PATH + 'cache'
+    TEMP_PATH         = BASE_PATH + 'temp'
+    CACHE_LIMIT_BYTES = 400 * 1024 ** 2
 
     def self.[](name)
       const_get(name.upcase)
--- lib/brandeins/utils/fetcher.rb
+++ lib/brandeins/utils/fetcher.rb
@@ -17,11 +17,8 @@ module BrandEins
 
       ContentNotFetchedError = Class.new StandardError
 
-      def initialize
-        @cache = {}
-      end
-
       def fetch(url)
+        remove_oldest_cache_file if cache_limit_reached?
         if cache_has_file?(url)
           cli.debug "Fetching file from cache: #{url}" do
             file_from_cache(url)
@@ -46,22 +43,25 @@ module BrandEins
       end
 
       def write_to_cache(file_name, file_content)
-        file_path = cache_path_for_file_name(file_name)
-        cli.debug "Writing file to cache: #{file_path}" do
-          !!File.binwrite(file_path, file_content)
+        cache_file_path = cache_path_for_file_name(file_name)
+        cli.debug "Writing file to cache: #{cache_file_path}" do
+          result = !!File.binwrite(cache_file_path, file_content)
+          add_to_cache(file_name, file_content)
+          return result
         end
       end
 
       def file_from_cache(file_name)
-        file_path = cache_path_for_file_name(file_name)
-        cli.statusline "Reading file from cache: #{file_path}" do
-          @cache[file_path] ||= File.binread(file_path)
+        cache_file_path = cache_path_for_file_name(file_name)
+        cli.statusline "Reading file from cache: #{file_name}" do
+          cache_file_path = cache_path_for_file_name(file_name)
+          File.binread(cache_file_path)
         end
       end
 
       def cache_has_file?(file_name)
-        file_path = cache_path_for_file_name(file_name)
-        @cache.key?(file_path) || File.exists?(file_path)
+        cache_path = cache_path_for_file_name(file_name)
+        cache_files.key? cache_path
       end
 
       def cache_path_for_file_name(file_name)
@@ -73,8 +73,37 @@ module BrandEins
         [uri.host, File.basename(uri.path)].compact.join('-')
       end
 
+      def cache_limit_in_bytes
+        BrandEins::Config['cache_limit_bytes']
+      end
+
       def cache_path
         BrandEins::Config['cache_path']
+      end
+
+      def cache_limit_reached?
+        cache_size_in_bytes > cache_limit_in_bytes
+      end
+
+      def cache_size_in_bytes
+        cache_files.reduce(0) { |sum, (file, _)| next unless file; sum += File.size?(file) || 0 }
+      end
+
+      def remove_oldest_cache_file
+        oldest_file = cache_files.sort_by { |file, time| time }.last.first
+        cli.debug "Removing cached file: #{oldest_file}" do
+          FileUtils.rm oldest_file
+          cache_files.delete(oldest_file)
+        end
+      end
+
+      def add_to_cache(file_name, file_content)
+        cache_file_path = cache_path_for_file_name(file_name)
+        cache_files[cache_file_path.to_s] = File.mtime(cache_file_path)
+      end
+
+      def cache_files
+        @cache_files ||= Hash[Dir[cache_path + './*'].map { |f| [ f, File.mtime(f) ] }]
       end
 
       def cli
